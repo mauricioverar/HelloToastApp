98ff729a6d2f25c097d1691b98296c01a43e1efe
Mauricio <stb539@gmail.com>
2024-04-11 17:22:23 -0400

    primer boton ok

--- app/src/main/java/com/example/hellotoastapp/MainActivity.java
+++ app/src/main/java/com/example/hellotoastapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hellotoastapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // recibir datos desde layout
+        //TextView txt1 =findViewById(R.id.num1);
+        Button btn1 = findViewById(R.id.button);
+        Button btn2 = findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+                // duracion toast=ventana emergente
+                Toast toast = Toast.makeText(getBaseContext(), text, duration);
+                toast.show();
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
